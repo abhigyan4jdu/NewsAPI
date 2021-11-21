29ee012f34d6954075ae08e07a43e254aefe2aa9
abhigyan4jdu <abhigyan4jdu@gmail.com>
2021-11-21 12:05:25 +0530

    View Set Updated

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import permissions, viewsets
 from .models import Post 
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+from django.contrib.auth import get_user_model 
 
-class PostList(generics.ListAPIView):
+
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+"""class PostList(generics.ListAPIView):
+   permission_classes = (permissions.IsAuthenticated, )
+   queryset = Post.objects.all()
+   serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAdminUser, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer"""
