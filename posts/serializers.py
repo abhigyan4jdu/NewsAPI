9e376c90c739fe040996f29b16470c669e3d418c
abhigyan4jdu <abhigyan4jdu@gmail.com>
2021-11-21 11:29:13 +0530

    Third Commit

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
 
 class PostSerializer(serializers.ModelSerializer):
 
@@ -15,3 +16,9 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
         )
         model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
